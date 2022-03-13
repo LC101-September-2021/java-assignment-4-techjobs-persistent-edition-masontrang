2857e4a6c53ecf63c1e813c4730408f2104c42e7
masontrang <mt9260@gmail.com>
2022-03-13 14:24:06 -0400

    Finishing Part 3

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,6 +1,11 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,6 +20,16 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    //Maybe not needed?
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private SkillRepository skillRepository;
+
+
     @RequestMapping("")
     public String index(Model model) {
 
@@ -27,6 +42,9 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("employers",employerRepository.findAll());
+        model.addAttribute("skills",skillRepository.findAll());
+
         return "add";
     }
 
@@ -34,11 +52,24 @@ public class HomeController {
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
                                        Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
 
+
+
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             return "add";
         }
 
+        //Add Job Code
+//        model.addAttribute("employer", employerRepository.findById(employerId));
+//        model.addAttribute("skills",skillRepository.findById(employerId))
+
+//        model.addAttribute("employers", employerRepository.findById(employerId));
+//        model.addAttribute("skills",skills);
+//        String employer = employerRepository.findById(employerId);
+        model.addAttribute("employer", employerRepository.findById(employerId));
+        model.addAttribute("skills",skills);
+
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Description is required")
+    @Size(max = 30, message = "Name must be less than 30 characters")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Skill() {}
 }
